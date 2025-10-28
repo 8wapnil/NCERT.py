d40937c44cd67e15f4420ee740f6f586a6dc5005
Swapnil Pradhan <swapnil-pradhan@swapnilpradhan.onmicrosoft.com>
2025-10-28 17:35:43 +0530

    Update .gitignore and improve list sorting in 18.py
    Changed .gitignore to ignore .DS_Store files in all directories. Moved sorting of input numbers in 18.py to after input collection for efficiency.

--- CBSE/Advanced Programming with Python/Linear List Manipulation/18.py
+++ CBSE/Advanced Programming with Python/Linear List Manipulation/18.py
@@ -3,8 +3,8 @@ nos = []
 while True:
 	try:
 		nos.append(float(input("Enter a number or press any alphabet  to stop: ")))
-		nos.sort()
 	except: break
 
+nos.sort()
 try: print("Found at position ", nos.index(float(input("Search..."))) + 1)
 except: print("Not found!")
